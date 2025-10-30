ea47fa20c439f62dede53f1ef0e42692dcc7da00
ahmdpolash <ahmedpolash732@gmail.com>
2025-10-30 12:56:43 +0600

    all cars page have completed

---
+++ src/app/(commonLayout)/cars/page.tsx
@@ -0,0 +1,14 @@
+import AllCars from "@/app/components/pages/AllCars";
+import Container from "@/app/components/shared/Container";
+
+const AllCarsPage = () => {
+  return (
+    <Container>
+      <div className="container mx-auto">
+        <AllCars />
+      </div>
+    </Container>
+  );
+};
+
+export default AllCarsPage;
--- src/app/components/ui/CarCard.tsx
+++ src/app/components/ui/CarCard.tsx
@@ -1,9 +1,9 @@
-'use client';
+"use client";
 
-import React from 'react';
-import { MapPin } from 'lucide-react';
-import { Car } from '@/types';
-import Image from 'next/image';
+import React from "react";
+import { MapPin } from "lucide-react";
+import { Car } from "@/types";
+import Image from "next/image";
 
 interface CarCardProps {
   car: Car;
@@ -29,42 +29,49 @@ const CarCard: React.FC<CarCardProps> = ({ car }) => {
       <div className="p-4">
         <h3 className="text-2xl font-bold text-gray-900 mb-2">{car.title}</h3>
         <p className="text-xl font-semibold text-gray-700 mb-4">{car.price}</p>
-        <div className="grid grid-cols-3 text-[16px] text-gray-600 mb-2 space-x-4">
+
+        <div className="flex items-center gap-2 justify-between text-[16px] text-gray-600 mb-2 space-x-4">
           <span className="flex items-center">
             <span className="mr-1">
-              <Image src="/icon/road-icon.png"
-                alt='setting-icon'
+              <Image
+                src="/icon/road-icon.png"
+                alt="setting-icon"
                 height={20}
                 width={20}
-                /></span> {car.mileage}
+              />
+            </span>{" "}
+            {car.mileage}
           </span>
           <span className="flex items-center">
             <span className="mr-1">
-              <Image src="/icon/gas-icon.svg"
-                alt='setting-icon'
+              <Image
+                src="/icon/gas-icon.svg"
+                alt="setting-icon"
                 height={20}
                 width={20}
-                />
-              </span> {car.transmission}
+              />
+            </span>{" "}
+            {car.transmission}
           </span>
           {car.isAuto && (
             <span className="flex items-center mr-4">
               <span className="mr-1">
-                <Image src="/icon/setting.svg"
-                alt='setting-icon'
-                height={20}
-                width={20}
+                <Image
+                  src="/icon/setting.svg"
+                  alt="setting-icon"
+                  height={20}
+                  width={20}
                 />
-                </span> Auto
+              </span>{" "}
+              Auto
             </span>
           )}
-          
         </div>
         <div>
-            <span className="flex items-center mb-2 text-gray-600 ">
+          <span className="flex items-center mb-2 text-gray-600 ">
             <MapPin size={20} className="mr-1 text-gray-600" /> {car.location}
           </span>
-          </div>
+        </div>
         <button className="w-full bg-[#00695C] text-white py-2 rounded-md hover:bg-teal-700 transition font-medium">
           View Details
         </button>
