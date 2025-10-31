f7543f7959a5009aa1c29fc3b527ac7a11bf3681
ahmdpolash <ahmedpolash732@gmail.com>
2025-10-31 15:56:21 +0600

    fixed issue

---
+++ src/constant/index.ts
@@ -0,0 +1,26 @@
+export const steps = [
+  {
+    title: "Verified Payments",
+    date: "Dec 15",
+    time: "10:04 AM",
+    address: "User Verified",
+  },
+  {
+    title: "Order is in Packing",
+    date: "Dec 15",
+    time: "15:20 PM",
+    address: "Warehouse A",
+  },
+  {
+    title: "Orders are On Train",
+    date: "Dec 15",
+    time: "11:30 AM",
+    address: "Station Port",
+  },
+  {
+    title: "Order Customs Port",
+    date: "Dec 16",
+    time: "14:40 PM",
+    address: "Port Location",
+  },
+];
